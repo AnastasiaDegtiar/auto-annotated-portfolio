fab84f3b9eedf435e606dcfefe7367744216a761
AnastasiaDegtiar <112903432+AnastasiaDegtiar@users.noreply.github.com>
2024-11-26 17:03:02 +0200

    Update index.tsx

--- src/components/layouts/BaseLayout/index.tsx
+++ src/components/layouts/BaseLayout/index.tsx
@@ -24,48 +24,47 @@ const BaseLayout: React.FC<BaseLayoutProps> = (props) => {
             <div className={classNames('sb-page', page?.colors || 'colors-a')}>
                 {page?.backgroundImage && <BackgroundImage {...page?.backgroundImage} />}
                 <div className="sb-base sb-default-base-layout relative">
-                    <Head>
-                        <title>{title}</title>
-                        {metaDescription && <meta name="description" content={metaDescription} />}
-                        {metaTags.map((metaTag) => {
-                            if (metaTag.format === 'property') {
-                                // OpenGraph meta tags (og:*) should be have the format <meta property="og:…" content="…">
-                                return <meta key={metaTag.property} property={metaTag.property} content={metaTag.content} />;
-                            }
-                            return <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />;
-                        })}
-                        <meta name="viewport" content="width=device-width, initial-scale=1" />
-                        {site.favicon && <link rel="icon" href={site.favicon} />}  
-                <script async src="https://stage-widget.intelswift.com/script.js?tenantId=cedd59ae-5e97-46c5-bbe8-08adc59deff5&botId=6744b67d85882dedc8b47e90&end=true"></script>
-				<script dangerouslySetInnerHTML={{
-    __html: `
-	window.onload = (event) => {
-						const propsInterval = setInterval(widgetTimer, 1000);
+                   <Head>
+    <title>{title}</title>
+    {metaDescription && <meta name="description" content={metaDescription} />}
+    {metaTags.map((metaTag) => {
+        if (metaTag.format === 'property') {
+            return <meta key={metaTag.property} property={metaTag.property} content={metaTag.content} />;
+        }
+        return <meta key={metaTag.property} name={metaTag.property} content={metaTag.content} />;
+    })}
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    {site.favicon && <link rel="icon" href={site.favicon} />}
+    <script async src="https://stage-widget.intelswift.com/script.js?tenantId=cedd59ae-5e97-46c5-bbe8-08adc59deff5&botId=6744b67d85882dedc8b47e90&end=true"></script>
+    <script
+        dangerouslySetInnerHTML={{
+            __html: `
+                window.onload = (event) => {
+                    const propsInterval = setInterval(widgetTimer, 1000);
 
-						function widgetTimer() {
-							const tenantId = localStorage.getItem("wws-tenant-id")
-							const botId = localStorage.getItem("wws-bot-id")
-							const host = window.location.hostname
-							const language = navigator.language || navigator.userLanguage; 
+                    function widgetTimer() {
+                        const tenantId = localStorage.getItem("wws-tenant-id")
+                        const botId = localStorage.getItem("wws-bot-id")
+                        const host = window.location.hostname
+                        const language = navigator.language || navigator.userLanguage; 
 
-							if((tenantId && tenantId != "undefined") && (botId && botId != "undefined") && (host && host != "undefined")){
-								clearInterval(propsInterval);
-								document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
-								{
-									tenantId: tenantId,
-									botId: botId,
-									host: host,
-									contact_language: language
-								},"*")
-							}
-						}
-					};
-   `
-	    }} />
+                        if((tenantId && tenantId != "undefined") && (botId && botId != "undefined") && (host && host != "undefined")){
+                            clearInterval(propsInterval);
+                            document.getElementById("iframeWidgetContainer").contentWindow.postMessage( 
+                            {
+                                tenantId: tenantId,
+                                botId: botId,
+                                host: host,
+                                contact_language: language
+                            },"*")
+                        }
+                    }
+                };
+            `,
+        }}
+    />
+</Head>
 
-				</script>
-                
-                    </Head>
                     {site.header && (
                         <Annotated content={site}>
                             <Annotated content={site.header}>
